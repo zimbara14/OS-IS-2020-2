8a6a6dad3828190bcf7b375cc3c4618cecf0f707
zimbara14 <winterking000@gmail.com>
2020-11-05 23:50:16 +0300

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ps -U user -o pid,command | tail -n +2 | sed -r "s/\s*([0-9]+)\s(.+)$/\1:\2/" > answer.txt
+#ps -U user -o pid,command | tail -n +2 | sed -r "s/\s*([0-9]+)\s(.+)$/\1:\2/" > answer.txt
+
+#ps -Admin o pid,user,command
+
+ps a -u admin --format pid,command | awk '{print $1":"$2}'
