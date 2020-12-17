01372adf774d3370663a37b347192cc7a0e688bd
Sara Nikolic <67017679+zimbara14@users.noreply.github.com>
2020-12-18 01:46:17 +0300

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps aux | awk '$1 == "root"{print $0}' > temp
+ps aux | awk '$1 == "user"{print $0}' > temp
 cat temp | wc -l > first.out
 awk '{printf "%s:%s\n", $2, $11}' temp >> first.out
 rm temp
